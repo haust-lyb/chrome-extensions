48310cbf0e369f9ea2ab345f3d0b87cf5f73c53e
李一博 <1570194845@qq.com>
2021-11-16 16:05:15 +0800

    Initial commit

---
+++ background.js
@@ -0,0 +1,6 @@
+let color = 'rgb(58, 167, 87)';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+});
